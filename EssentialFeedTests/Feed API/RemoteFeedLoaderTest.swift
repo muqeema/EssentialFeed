a076189d321cd42b4ec11aecaaebcb3ca464463d
Muqeem <muqeem.ahmad@doceree.com>
2023-04-29 18:54:06 +0530

    Simplify type declaration with a helper function

--- EssentialFeedTests/Feed API/RemoteFeedLoaderTest.swift
+++ EssentialFeedTests/Feed API/RemoteFeedLoaderTest.swift
@@ -38,7 +38,7 @@ class RemoteFeedLoaderTests: XCTestCase {
     func test_load_deliversErrorOnClientError() {
         let (sut, client) = makeSUT()
         
-        expect(sut, toCompleteWith: .failure(RemoteFeedLoader.Error.connectivity), when:  {
+        expect(sut, toCompleteWith: failure(.connectivity), when:  {
             let clientError = NSError(domain: "Test", code: 0)
             client.completion(with: clientError)
         })
@@ -52,7 +52,7 @@ class RemoteFeedLoaderTests: XCTestCase {
         
         samples.enumerated().forEach { index, code in
             
-            expect(sut, toCompleteWith: .failure(RemoteFeedLoader.Error.invalidData), when: {
+            expect(sut, toCompleteWith: failure(.invalidData), when: {
                 let json = makeItemsJSON([])
                 client.complete(withStatusCode: code, data: json, at: index)
             })
@@ -114,6 +114,10 @@ class RemoteFeedLoaderTests: XCTestCase {
         trackForMemoryLeaks(sut)
         trackForMemoryLeaks(client)
         return (sut, client)
+    }
+    
+    private func failure(_ error: RemoteFeedLoader.Error) -> RemoteFeedLoader.Result {
+        return .failure(error)
     }
     
     private func trackForMemoryLeaks(_ instance: AnyObject, file: StaticString = #file, line: UInt = #line) {
